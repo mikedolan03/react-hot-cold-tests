c38e4d0e468fd688ec70c132d3b6b986d9adc7dd
MIKES-DELL-LAPT\mikedolan03 <mikedolan03@gmail.com>
2018-06-06 16:33:56 -0400

    adding tests

---
+++ src/components/guess-form-test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import GuessForm from './guess-form';
+
+describe('<GuessForm />', () => {
+    it('Renders without crashing', () => {
+        shallow(<GuessForm />);
+    });
